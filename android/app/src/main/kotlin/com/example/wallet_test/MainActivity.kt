d3409c139f3cc1c79ef1a51d55df23c18b53a668
Chiara-Vanderlan <115263438+Chiara-Vanderlan@users.noreply.github.com>
2023-08-05 21:02:46 +0530

    adding packages

--- android/app/src/main/kotlin/com/example/wallet_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/wallet_test/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.wallet_test
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
